5d1b14f6234e9dca77a52a74ea6be7416b302517
Yusef Ashjaee <mehr.ashjaee@gmail.com>
2020-09-02 00:01:32 +0430

    Updates

---
+++ precache-manifest.75de2d9de19029e0eac62466ce29bdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86b6dfa92a0591d7d2750735f7a8ef7",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "688cef54295ab2828962",
+    "url": "/shop/static/css/main.3d3872f2.chunk.css"
+  },
+  {
+    "revision": "2ef6262174e6bf3f9cb6",
+    "url": "/shop/static/js/2.4b8d3738.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/shop/static/js/2.4b8d3738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688cef54295ab2828962",
+    "url": "/shop/static/js/main.9910929e.chunk.js"
+  },
+  {
+    "revision": "8e867c3173a1dac9de87",
+    "url": "/shop/static/js/runtime-main.262d7316.js"
+  }
+]);
